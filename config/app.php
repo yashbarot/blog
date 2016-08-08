bf35c9f6692da70452a8eb1668d4543b5b104b3a
Yash Barot <yashce04@gmail.com>
2016-08-08 19:11:23 +0530

    Changed Blog config file

--- config/app.php
+++ config/app.php
@@ -203,17 +203,4 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Blog Application config
-    |--------------------------------------------------------------------------
-    |
-    | Blog title and post per page configuration
-    |
-    */
-
-    'title' => 'My Blog',
-    'post_per_page' => 10
-
 ];
